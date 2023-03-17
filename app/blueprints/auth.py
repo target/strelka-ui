d03d9a496caee3dd8d77228bdc818088e15c4f2d
Paul Hutelmyer <paul.hutelmyer@gmail.com>
2023-03-17 12:59:31 -0400

    Updating auth

--- app/blueprints/auth.py
+++ app/blueprints/auth.py
@@ -1,7 +1,6 @@
 """
 Authentication controller
 """
-
 from datetime import datetime, timedelta
 from random import choice
 from string import ascii_letters, digits
@@ -39,7 +38,9 @@ def get_api_key(user):
             db.session.delete(existing_key)
             db.session.commit()
     key = "".join(choice(ascii_letters + digits) for _ in range(32))
-    expiration = datetime.now() + timedelta(days=30)
+    expiration = datetime.now() + timedelta(
+        days=int(current_app.config["API_KEY_EXPIRATION"])
+    )
     api_key = ApiKey(key=key, user_cn=user.user_cn, expiration=expiration)
     db.session.add(api_key)
     db.session.commit()
@@ -64,15 +65,15 @@ def login():
             instance={"username": username, "password": password}, schema=loginSchema
         )
     except ValidationError as err:
-        #current_app.logger.error("Failed login validation: %s", err)
+        # current_app.logger.error("Failed login validation: %s", err)
         return jsonify({"error": "Failed to validate username/password"}), 400
 
-    #current_app.logger.info("received login attempt for user %s", username)
+    # current_app.logger.info("received login attempt for user %s", username)
     success, ldapUser = check_credentials(username, password)
     if not success:
         return jsonify({"error": "incorrect username/password combination"}), 401
 
-    #current_app.logger.info("ldap auth suceeded for user %s", ldapUser["user_cn"])
+    # current_app.logger.info("ldap auth suceeded for user %s", ldapUser["user_cn"])
     session["user_cn"] = ldapUser["user_cn"]
     session["first_name"] = ldapUser["first_name"]
     session["last_name"] = ldapUser["last_name"]
@@ -102,7 +103,7 @@ def login():
         session["user_id"] = dbUser.id
         session["logged_in"] = True
     except Exception as err:
-        #current_app.logger.error("Failed connection to database: %s", err)
+        # current_app.logger.error("Failed connection to database: %s", err)
         return jsonify({"error": "Failed to connect to database"}), 400
 
     return (
