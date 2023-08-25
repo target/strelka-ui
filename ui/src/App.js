d7b551864d62c50ac18b953f6639ddf9e56542af
Paul Hutelmyer <paul.hutelmyer@gmail.com>
2023-08-25 12:49:41 -0400

    Adding dev

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import { BrowserRouter } from 'react-router-dom'
-import MainRouter from './routes/MainRouter'
-import AuthenticationProvider from './providers/AuthProvider';
+import { BrowserRouter } from "react-router-dom";
+import MainRouter from "./routes/MainRouter";
+import AuthenticationProvider from "./providers/AuthProvider";
 
-class App extends Component {  
+const baseUrl = window.appConfig ? window.appConfig.baseUrl : "/";
+
+class App extends Component {
   render() {
     return (
       <div className="App">
         <AuthenticationProvider>
-          <BrowserRouter>
-            <MainRouter/>
+          <BrowserRouter basename={baseUrl}>
+            <MainRouter />
           </BrowserRouter>
         </AuthenticationProvider>
       </div>
