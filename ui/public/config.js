8f157d538ce172ef7f85bcd3ed87bcda6f9ca456
Paul Hutelmyer <paul.hutelmyer@gmail.com>
2023-09-08 13:57:58 -0400

    Update config.js

--- ui/public/config.js
+++ ui/public/config.js
@@ -1,3 +1,3 @@
 window.appConfig = {
-    baseUrl: '/strelkaui' // Default to strelkaui for now. This can be changed to '/whatever' or any other base path as needed.
+    baseUrl: '/strelkaui' // Default to strelkaui for now.
 };
