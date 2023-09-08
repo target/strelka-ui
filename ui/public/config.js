712370e2b36b3f2fef6c6f5166091ba0e6850514
Paul Hutelmyer <paul.hutelmyer@gmail.com>
2023-09-08 14:03:20 -0400

    Update config.js

--- ui/public/config.js
+++ ui/public/config.js
@@ -1,3 +1,3 @@
 window.appConfig = {
-    baseUrl: '/strelkaui' // Default to strelkaui for now.
+    baseUrl: '/strelkaui' // Change this to modify the base_url to be used as a prefix to routes.
 };
